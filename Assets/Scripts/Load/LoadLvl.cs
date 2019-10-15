1479e8016b174a9a5eb6145689dd17fba2d96dd6
blizzard190 <22256@ma-web.nl>
2019-10-15 13:53:54 +0200

    Animations + button script
    Used https://youtu.be/vqZjZ6yv1lA as tutorial

---
+++ Assets/Scripts/Load/LoadLvl.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class LoadLvl : MonoBehaviour
+{
+    [SerializeField] Image _Progressbar;
+
+    public void StartLoad(int lvl)
+    {
+        StartCoroutine(LoadAsyncOperation(lvl));
+    }
+
+    IEnumerator LoadAsyncOperation(int lvl)
+    {
+        AsyncOperation gameLevel = SceneManager.LoadSceneAsync(lvl);
+
+        while(gameLevel.progress < 1)
+        {
+            _Progressbar.fillAmount = gameLevel.progress;
+            yield return new WaitForEndOfFrame();
+        }
+    }
+}
---
+++ Assets/Scripts/UI/ButtonManager.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonManager : MonoBehaviour
+{
+    public int index;
+
+    [SerializeField] bool _Keydown;
+    [SerializeField] int _MaxIndex;
+
+    void Update()
+    {
+        if (Input.GetAxis("Vertical") != 0)
+        {
+            if (!_Keydown)
+            {
+                _Keydown = true;
+                if (Input.GetAxis("Vertical") < 0)
+                {
+                    if (index < _MaxIndex)
+                    {
+                        index++;
+                    }
+                    else
+                    {
+                        index = 0;
+                    }
+                }
+                else if (Input.GetAxis("Vertical") > 0)
+                {
+                    if (index > 0)
+                    {
+                        index--;
+                    }
+                    else
+                    {
+                        index = _MaxIndex;
+                    }
+                }
+            }
+        }
+        else
+        {
+
+            _Keydown = false;
+        }
+    }
+}
