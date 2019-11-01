82bd89b0c58f3de4d793829607a64ef43d91e2c2
blizzard190 <22256@ma-web.nl>
2019-11-01 11:31:45 +0100

    Start aan menu switch

---
+++ Assets/Scripts/UI/UISceneSwitch.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UISceneSwitch : MonoBehaviour
+{
+    public List<ButtonManager> UIScenes;
+    private int _LastActive = 0;
+    private int _Active = 0;
+    private ButtonManager _Manager;
+
+    void Update()
+    {
+        _LastActive = _Active;
+        _Manager = UIScenes[_Active].GetComponent<ButtonManager>();
+        _Manager.CheckActive();
+    }
+}
